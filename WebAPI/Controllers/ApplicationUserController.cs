0bbb46e2d754ef749bb28d5ce1a1429cab341338
sindatalbi <sinda.talbi@esprit.tn>
2019-08-29 15:16:13 +0100

    commit1

---
+++ WebAPI/Controllers/ApplicationUserController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net.Mail;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using WebAPI.Models;
+using WebAPI.Models.Response;
+using WebAPI.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApplicationUserController : ControllerBase
+    {
+        private UserManager<ApplicationUser> _userManager;
+        private SignInManager<ApplicationUser> _singInManager;
+        private readonly ApplicationSettings _appSettings;
+        private readonly IEmailSender _emailSender;
+
+        public ApplicationUserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, IOptions<ApplicationSettings> appSettings, IEmailSender emailSender)
+        {
+            _userManager = userManager;
+            _singInManager = signInManager;
+            _appSettings = appSettings.Value;
+            _emailSender = emailSender;
+        }
+
+
+        [HttpPost]
+        [Route("Register")]
+        //POST : /api/ApplicationUser/Register
+        public async Task<Object> PostApplicationUser(RegisterUserModel model)
+        {
+            model.Role = "Admin";
+            var applicationUser = new ApplicationUser() {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName,
+                IsActive = true
+            };
+
+         
+                var result = await _userManager.CreateAsync(applicationUser, model.Password);
+                if (result.Succeeded)
+                {
+                    //await _signInManager.SignInAsync(user, false);
+                    //var token = AuthenticationHelper.GenerateJwtToken(model.Email, user, _configuration);
+
+                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(applicationUser);
+                    string codeEncoded = HttpUtility.UrlEncode(code);
+
+                    var callbackUrl = new Uri(@"http://localhost:4200/user/login" + applicationUser.Email + "&" + codeEncoded);
+
+                    string subject = "account confirmation";
+                    string body = "Please confirm your account by clicking " + " <a href='" + callbackUrl + "'>here</a>";
+
+                    //SEND EMAIL///                          
+
+                    SmtpClient client = new SmtpClient("smtp.gmail.com");
+
+                    _emailSender.Send(model.Email, body, subject);
+
+                    /////
+                   
+                }
+                await _userManager.AddToRoleAsync(applicationUser, model.Role);
+                return Ok(result);
+            }
+        
+        [HttpPost]
+        [Route("Login")]
+        //POST : /api/ApplicationUser/Login
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            var user = await _userManager.FindByNameAsync(model.UserName);
+            if (user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                //Get role assigned to the user
+                var role = await _userManager.GetRolesAsync(user);
+                IdentityOptions _options = new IdentityOptions();
+
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new Claim[]
+                    {
+                        new Claim("UserID",user.Id.ToString()),
+                        new Claim("UserName",user.UserName),
+                        new Claim("FullName",user.FullName),
+                        new Claim(_options.ClaimsIdentity.RoleClaimType,role.FirstOrDefault())
+                    }),
+                    Expires = DateTime.UtcNow.AddDays(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_appSettings.JWT_Secret)), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var tokenHandler = new JwtSecurityTokenHandler();
+                var securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                var token = tokenHandler.WriteToken(securityToken);
+
+                return Ok(new { token,user.Id, user.UserName });
+            }
+            else
+                return BadRequest(new { message = "Username or password is incorrect." });
+        }
+
+    }
+}
---
+++ WebAPI/Models/Response/SignUpResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models.Response
+{
+    public class SignUpResponse
+    {
+        public string fullname { get; private set; }
+        public string Email { get; private set; }
+
+        public SignUpResponse(string name, string email)
+        {
+            fullname = name;
+            Email = email;
+
+
+        }
+
+
+    }
+}
